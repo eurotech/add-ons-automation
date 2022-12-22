2ae2a474e590759beacab4559c2489b2a55ef7c3
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-22 14:50:57 +0100

    fix: fixed manifest builder download URL

--- samples/manifest-builder.groovy
+++ samples/manifest-builder.groovy
@@ -55,7 +55,7 @@ node ("rhel-large") {
 
             // Download and run python script
             sh """
-                curl wget https://raw.githubusercontent.com/eurotech/add-ons-automation/feat/manifest_builder/config/manifest_builder/manifest_builder.py --output manifest_builder
+                curl wget https://raw.githubusercontent.com/eurotech/add-ons-automation/main/config/manifest_builder/manifest_builder.py --output manifest_builder
                 python3 manifest_builder.py -f ${uploadDirectory} -v ${pomVersion} -n ${pomProjectName} -b ${BUILD_NUMBER} -c ${descriptorsFile}
             """
         }
